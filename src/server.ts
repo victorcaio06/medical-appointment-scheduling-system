ca3204e8e34f76dc3951e69310a809f6b48f6bbe
Victor Caio <kaioaraujo426@gmail.com>
2023-02-23 03:17:04 -0300

    BASE STRUCTURE

---
+++ src/server.ts
@@ -0,0 +1,10 @@
+import express from "express";
+
+const app = express();
+const port = 3434;
+
+// app.get("/", (request, response) => {
+  
+// });
+
+app.listen(port, () => console.log("Server is running on PORT 3434!"));
