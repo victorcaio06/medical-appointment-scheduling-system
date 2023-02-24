a138ce53e8d2690e21eb5ceccb57d686d06fbf69
Victor Caio <kaioaraujo426@gmail.com>
2023-02-24 00:28:37 -0300

    FEAT: added Winston and configured

--- src/useCases/createUser/createUserController.ts
+++ src/useCases/createUser/createUserController.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from "express";
+import { logger } from "../../utils/logger";
 import { CreateUserUseCase } from "./createUserUseCase";
 
 export class CreateUserController {
@@ -12,6 +13,8 @@ export class CreateUserController {
 
       return response.status(201).json(result);
     } catch (err: any) {
+      logger.error(err.stack);
+
       response.status(404).json({
         message: err.message,
       });
