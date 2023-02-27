3bd3f7e33f9ac255a234c474c40db118d49117d9
Victor Caio <kaioaraujo426@gmail.com>
2023-02-27 10:04:35 -0300

    CHORE: added verification for connection with database

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,6 @@
 import express from "express";
+import { CustomError } from "./errors/custom.error";
+import { prismaClient } from "./infra/database/prisma.config";
 import { specialtyRouter } from "./routes/specialty.routes";
 import { userRouter } from "./routes/user.routes";
 
@@ -10,5 +12,11 @@ const port = 3434;
 
 app.use(userRouter);
 app.use(specialtyRouter);
+
+try {
+  prismaClient.$connect();
+} catch (err) {
+  throw new CustomError("SERVER OFF", 500, "SERVER_OFF_ERROR");
+}
 
 app.listen(port, () => console.log("Server is running on PORT 3434!"));
