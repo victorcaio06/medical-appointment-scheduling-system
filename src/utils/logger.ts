79dcb010d0fdad1b21ee4911bf52803cc0858931
Victor Caio <kaioaraujo426@gmail.com>
2023-02-27 10:05:14 -0300

    CHORE: added level info for logger

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -2,12 +2,13 @@ import winston from "winston";
 
 const logger = winston.createLogger({
   format: winston.format.combine(
-    winston.format.timestamp({format: "DD/MM/YYY HH:mm:ss"}),
+    winston.format.timestamp({ format: "DD/MM/YYY HH:mm:ss" }),
     winston.format.json()
   ),
   transports: [
     new winston.transports.File({
       filename: "logs/app.log",
+      level: "info",
     }),
     new winston.transports.File({
       filename: "logs/error.log",
