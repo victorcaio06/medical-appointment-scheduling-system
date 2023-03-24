44c2a9851678a3213a1cfef6dbba0a0eea940fdd
Victor Caio <kaioaraujo426@gmail.com>
2023-03-24 09:57:27 -0300

    FEAT: added nodemailer, ethereal and configured

---
+++ src/infra/providers/mail/implementations/ethereal.mail.provider.ts
@@ -0,0 +1,40 @@
+import nodemailer, { Transporter } from "nodemailer";
+
+import { IMailProvider, MailDTO } from "../mail.provider";
+
+export class EtherealMailProvider implements IMailProvider {
+  private client!: Transporter;
+
+  constructor() {
+    nodemailer
+      .createTestAccount()
+      .then(() => {
+        const transporter = nodemailer.createTransport({
+          host: "smtp.ethereal.email",
+          port: 587,
+          auth: {
+            user: "sofia.blick@ethereal.email",
+            pass: "veRaAPBgQ9zadKRDUB",
+          },
+        });
+
+        this.client = transporter;
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  async sendEmail(data: MailDTO): Promise<void> {
+    const resultMail = await this.client.sendMail({
+      to: data.to,
+      from: data.from,
+      subject: data.subject,
+      text: data.text,
+      html: data.html,
+    });
+
+    console.log("Message send: %s", resultMail.messageId);
+    console.log("Preview URL: %s", nodemailer.getTestMessageUrl(resultMail));
+  }
+}
