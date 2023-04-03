b72c6e20eaae33d39e7c8d862431dfaf211dceaa
Victor Caio <kaioaraujo426@gmail.com>
2023-04-03 09:26:54 -0300

    CHORE: added and configured Multer

---
+++ src/config/upload.config.ts
@@ -0,0 +1,17 @@
+import multer from "multer";
+import { resolve } from "path";
+import { randomBytes } from "crypto";
+
+const folderTmp = resolve(__dirname, "..", "..", "tmp");
+
+export default {
+  storage: multer.diskStorage({
+    destination: folderTmp,
+    filename: (request, file, callback) => {
+      const fileHash = randomBytes(16).toString("hex");
+      const fileName = `${file.originalname}-${fileHash}`;
+
+      return callback(null, fileName);
+    },
+  }),
+};
