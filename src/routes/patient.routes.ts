51353a540d3db98820eac3f9ab2cd0e6a0ad599d
Victor Caio <kaioaraujo426@gmail.com>
2023-04-03 09:28:00 -0300

    FEAT: changed route to accept avatar

--- src/routes/patient.routes.ts
+++ src/routes/patient.routes.ts
@@ -1,9 +1,14 @@
 import { Router } from "express";
+import multer from "multer";
+
 import { createPatientController } from "../modules/patients/useCases/createPatient";
+import uploadConfigMulter from "../config/upload.config";
 
 const patientRouter = Router();
 
-patientRouter.post("/patients", async (request, response) => {
+const uploadFile = multer(uploadConfigMulter);
+
+patientRouter.post("/patients", uploadFile.single("avatar"), async (request, response) => {
   await createPatientController.handle(request, response);
 });
 
