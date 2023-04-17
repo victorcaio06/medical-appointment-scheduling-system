1bda210a4637dd84b7852a9970442568ae15a5a5
Victor Caio <kaioaraujo426@gmail.com>
2023-04-17 08:50:17 -0300

    REFACTOR: removed unnecessary console.log

--- src/infra/cron/notification-appointments-day.cron.ts
+++ src/infra/cron/notification-appointments-day.cron.ts
@@ -3,8 +3,6 @@ import cron from "node-cron";
 import { AppointmentPrismaRepository } from "../../modules/appointments/repositories/prisma/appointment.prisma.repository";
 import { CreateNotificationAppointmentUseCase } from "../../modules/appointments/useCases/createNotificationAppointment/create-notification-appointment.useCase";
 
-console.log("entrou aqui no cron");
-
 cron.schedule("0 0 0 * * *", async () => {
   console.log("Schedule is running!");
 
